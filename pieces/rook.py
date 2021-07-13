be4abb0b1d1625578bcde7b30a06516c85caefd0
awenelo <56133692+awenelo@users.noreply.github.com>
2021-07-13 08:26:38 -0400

    Remove automatically making board-exiting moves legal

--- pieces/rook.py
+++ pieces/rook.py
@@ -4,21 +4,23 @@ from .piece import Piece
 import configs
 
 class Rook(Piece):
-    # Overwrite the __init__ function to pass different images without input from main.py
+    # Overwrite the __init__ function to pass different images
     def __init__(self, startingsquare, isWhite):
         super().__init__(pygame.image.load("images/rook-piece-white.png"),
                          pygame.image.load("images/rook-piece-black.png"),
                          isWhite,
                          startingsquare)
+        
     # Function to check if move is leagal, overwrites the default function
     def is_valid_move(self, targetSquare, gamePieces, board, capture=False):
         # Check that Piece doesn't have something against moving here
         if not super().is_valid_move(targetSquare, gamePieces, board, capture=capture):
             return False
-        
-        # Check that the piece is on the board or if it's moving to the board, if it's not, make the move valid no matter what
-        if not board.is_on_board(self.rect.center) or not board.is_on_board((targetSquare[0]*configs.SQUARE_SIZE, targetSquare[1]*configs.SQUARE_SIZE)):
-            return True
+
+        # Removed, because Pieces.update() prevents exiting the board
+        ## Check that the piece is on the board or if it's moving to the board, if it's not, make the move valid no matter what, if we're not capturing
+        ##if not board.is_on_board(self.rect.center) or not board.is_on_board((targetSquare[0]*configs.SQUARE_SIZE, targetSquare[1]*configs.SQUARE_SIZE)):
+        ##    return True
 
         # Check that we're moving to a square with the same horizontal or vertical position
         if not(self.squarex == targetSquare[0] or self.squarey == targetSquare[1]):
