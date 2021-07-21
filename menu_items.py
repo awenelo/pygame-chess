446e9b087bd5b9cfc9ae2e947e61ac7fddb5c9e9
awenelo <56133692+awenelo@users.noreply.github.com>
2021-07-21 09:53:33 -0400

    Remove repeated code from menu_items

--- menu_items.py
+++ menu_items.py
@@ -2,50 +2,40 @@ import pygame
 
 import configs
 
-# Classes that contain items for the menus
-class StartButton(pygame.sprite.Sprite):
-    def __init__(self):
-        # Initialize the sprite object
+# Class for all menu items
+class MenuItem(pygame.sprite.Sprite):
+    def __init__(self, image, size, center):
+        # Initiallize the Sprite class
         super().__init__()
         
-        # Load the start button image, and resize it to 75x50
+        # Load the image, and resize it to the correct size
         self.image = pygame.transform.smoothscale(
-            pygame.image.load("images/start_button.png"),
-            (200, 50)
+            pygame.image.load(image),
+            size
             )
         self.rect = self.image.get_rect()
-
-        # Move the rect into position
-        self.rect.center = (configs.WIDTH//2, configs.HEIGHT//2+125)
-
+        self.rect.center = center
+    
     def draw(self, screen):
         # Draw us on the screen
         screen.blit(self.image, self.rect)
 
     def click(self, game, menu):
-        # Start the game on click
+        # Handle anytime we're clicked on
+        pass
+
+# Classes that contain items for the menus
+class StartButton(MenuItem):
+    def __init__(self):
+        # Initialize the sprite object
+        super().__init__("images/start_button.png", (200, 50), (configs.WIDTH//2, configs.HEIGHT//2+125))
+
+    def click(self, game, menu):
+        # Overwrite click to start the game
         menu.game_menu()
         game.start_game()
 
-class TitleText(pygame.sprite.Sprite):
+class TitleText(MenuItem):
     def __init__(self):
         # Initialize the sprite object
-        super().__init__()
-        
-        # Load the start button image, and resize it to 300x210
-        self.image = pygame.transform.smoothscale(
-            pygame.image.load("images/title_text.png"),
-            (300, 210)
-            )
-        self.rect = self.image.get_rect()
-
-        # Move the rect into position
-        self.rect.midtop = (configs.WIDTH//2, 75)
-
-    def draw(self, screen):
-        # Draw us on the screen
-        screen.blit(self.image, self.rect)
-
-    def click(self, game, menu):
-        # Right now, do nothing on click
-        pass
+        super().__init__("images/title_text.png", (300, 210), (configs.WIDTH//2, 180))
